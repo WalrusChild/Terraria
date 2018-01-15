e31eb28a69675d40333483d9e3444586647ea5a7
WalrusChild <35446474+WalrusChild@users.noreply.github.com>
2018-01-15 00:33:47 -0700

    Add files via upload

---
+++ Heal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using PluginLoader;
+using Terraria;
+
+namespace WalrusPlugins
+{
+    public class Heal : MarshalByRefObject, IPluginChatCommand
+    {
+        private Keys healKey;
+
+        public Heal()
+        {
+            if (!Keys.TryParse(IniAPI.ReadIni("Heal", "healKey", "H", writeIt: true), out healKey))
+            {
+                healKey = Keys.H;
+            }
+
+            Loader.RegisterHotkey(() => HealPlayer(), healKey, ignoreModifierKeys: true);
+        }
+
+        private void HealPlayer()
+        {
+            var player = Main.player[Main.myPlayer];
+            int amt = player.statLifeMax2 - player.statLife;
+            player.statLife += amt;
+            player.HealEffect(amt, true);
+            Main.NewText("Healed " + amt.ToString() + ".");
+        }
+
+        public bool OnChatCommand(string command, string[] args)
+        {
+            if (command != "heal") 
+            {
+                return false;
+            }
+            HealPlayer();
+            return true;
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using PluginLoader;
+using Terraria;
+
+namespace WalrusPlugins
+{
+    public class Weather : MarshalByRefObject, IPluginChatCommand
+    {
+        private Keys clearKey, rainKey, slimeKey;
+
+        public Weather()
+        {
+            if (!Keys.TryParse(IniAPI.ReadIni("Weather", "Clear", "NumPad1", writeIt: true), out clearKey))
+                clearKey = Keys.NumPad1;
+            if (!Keys.TryParse(IniAPI.ReadIni("Weather", "Rain", "NumPad2", writeIt: true), out rainKey))
+                rainKey = Keys.NumPad2;
+            if (!Keys.TryParse(IniAPI.ReadIni("Weather", "Slime", "NumPad3", writeIt: true), out slimeKey))
+                slimeKey = Keys.NumPad3;
+
+            Loader.RegisterHotkey(() => ChangeWeather("clear"), clearKey, ignoreModifierKeys: true);
+            Loader.RegisterHotkey(() => ChangeWeather("rain"), rainKey, ignoreModifierKeys: true);
+            Loader.RegisterHotkey(() => ChangeWeather("slime"), slimeKey, ignoreModifierKeys: true);
+        }
+
+        private void ChangeWeather(string weather)
+        {
+            switch (weather)
+            {
+                case "clear":
+                    Main.StopRain();
+                    Main.StopSlimeRain();
+                    Main.NewText("Rained cleared.");
+                    break;
+
+                case "rain":
+                    Main.StartRain();
+                    Main.NewText("Rain started.");
+                    break;
+
+                case "slime":
+                    Main.StartSlimeRain();
+                    Main.NewText("Slime rain started.");
+                    break;
+
+                default:
+                    Main.NewText("Usage:");
+                    Main.NewText("    /weather clear");
+                    Main.NewText("    /weather rain");
+                    Main.NewText("    /weather slime");
+                    Main.NewText("    /weather help");
+                    break;
+            }
+        }
+
+        public bool OnChatCommand(string command, string[] args)
+        {
+            if (command != "weather") return false;
+
+            if (args.Length < 1 || args.Length > 1 || args[0] == "help")
+            {
+                Main.NewText("Usage:");
+                Main.NewText("    /weather clear");
+                Main.NewText("    /weather rain");
+                Main.NewText("    /weather slime");
+                Main.NewText("    /weather help");
+                return true;
+            }
+            return true;
+        }
+}
+}
